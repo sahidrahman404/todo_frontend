f157210566763750effc191996c0282d5c39b780
Sahid <sahidrahman404@gmail.com>
2024-06-06 06:40:19 +0700

    update todo page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { AddTodo } from "@/components/todo/AddTodo";
+import { TodoHeader } from "@/components/todo/TodoHeader";
 import Todos from "@/components/todo/Todos";
 
 export default async function Home() {
   return (
     <main className="mt-5 space-y-8 sm:w-full md:w-[500px] mx-auto">
+      <TodoHeader />
       <div className="flex justify-center">
         <AddTodo />
       </div>
