8b2c955034ab1fe2c27736d3d1c6efe0a24dcf20
Sahid <sahidrahman404@gmail.com>
2024-06-06 06:40:42 +0700

    add isDateAlmostDue function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,16 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const fetchWithZod = createZodFetcher();
+
+export function isDateAlmostDue(dueDate: Date, threshold: number = 1) {
+  const due = new Date(dueDate);
+
+  const now = new Date();
+
+  const timeDiff = due.getTime() - now.getTime();
+
+  const thresholdMs = threshold * 24 * 60 * 60 * 1000; // Threshold in days
+
+  // Check if the time difference is less than or equal to the threshold
+  return timeDiff <= thresholdMs;
+}
